dbd0fe0760adda32f4618f0d37c974d66f714f58
MohamedNasr95-github <143353170+MohamedNasr95@users.noreply.github.com>
2025-09-08 15:54:36 +0300

    Adding rules and bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Dawry-Al-Thanaweya-Baneen
+
+## Game Rules:
+    1. Players have 2 matches per week.
+    2. Matches will be played with best of 3 system.
+    3. Round consists of 10 points.
+    4. Players have to choose and accept 2 Judges for the match.
+    5. Both players have to accept judges decisions. 
+    6. Judges must focus on stepping on the line.
+    7. Every player plays 2 matches with every opponent
+    
+## Points distribution:
+    1. Winning a match 2-0 >> 2 points.
+    2. Winning a match 2-1 >> 1 point.
+    3. Losing a match      >> 0 points.
+
+## Positions:
+    1. Positions are determined based on points.
+    2. If 2 
--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 table = document.getElementById("tbody")
 
 // ========================   Data   ===============================================================================================
-players = [{ID: 1, name: "Mohamed Nasr", position: 1, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
-    {ID: 2, name: "Mohy Mahmoud", position: 1, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
-    {ID: 3, name: "Mohamed Fayed", position: 4, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
-    {ID: 4, name: "Ahmed Magdy", position: 2, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
-    {ID: 5, name: "Mahmoud Meredian", position: 3, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
-    {ID: 6, name: "Abdelrahman Meredian", position: 5, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
-    {ID: 7, name: "Mohamed Osama", position: 7, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
+players = [{ID: 1, name: "Mohamed Nasr", position: 0, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
+    {ID: 2, name: "Mohy Mahmoud", position: 0, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
+    {ID: 3, name: "Mohamed Fayed", position: 0, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
+    {ID: 4, name: "Ahmed Magdy", position: 0, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
+    {ID: 5, name: "Mahmoud Meredian", position: 0, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
+    {ID: 6, name: "Abdelrahman Meredian", position: 0, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
+    {ID: 7, name: "Mohamed Osama", position: 0, points: 0, win: 0, loss: 0, rWin: 0, rLost: 0, pGained: 0, pLost:0},
 ]
 // === sorting by position ===
 players.sort(function(a, b){return a.position - b.position})
